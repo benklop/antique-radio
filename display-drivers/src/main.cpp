7bae78a370189a17aec3aa9f2b7b87ed6fb73e77
Ben Klopfenstein <benklop@gmail.com>
2018-07-23 01:46:47 -0600

    initial image testing

--- display-drivers/src/main.cpp
+++ display-drivers/src/main.cpp
@@ -2,9 +2,62 @@
 #include "../driver/Noritake_VFD_GU7000.h"
 #include "Config.h"
 
+//libraries
+#include "INIReader.h"
+#include "../cimg/CImg.h"
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <bitset>
+
 Noritake_VFD_GU7000 vfd;
+
+using namespace std;
+using namespace cimg_library;
+
+struct gu7000_image {
+    uint8_t * data;
+    int width;
+    int height;
+};
+
+gu7000_image load_image(string filename) {
+    cout << "loading image " << filename << " ... ";
+    CImg<bool> src(filename.c_str());
+
+    gu7000_image image;
+    image.width = src.width();
+    image.height = src.height();
+
+    uint8_t data[image.width * image.height];
+    for (int i=0; i < (image.width * image.height) / 8; i++) {
+        for(int j=0; j < 8; j++) {
+            int this_bit = i*8 + j;
+            int col = this_bit + 1 % (image.width);
+            int row = this_bit + 1 / (image.width);
+            if(*src.data(row, col) == true) {
+                data[i] |= (1<<j);
+            }
+        }
+    }
+    image.data = data;
+    cout << "success!" << endl;
+    return image;
+}
+
 int main() {
     vfd.GU7000_reset();
     vfd.GU7000_init();
-    vfd.GU7000_drawFROMImage(0x0, 16, 16, 16); // Display the image antenna 16X16
+   
+
+    INIReader reader("config.ini");
+    if (reader.ParseError() < 0) {
+        cout << "Can't load 'config.ini'\n";
+        return 1;
+    }
+
+    gu7000_image image = load_image(reader.Get("images", "initial", ""));
+    vfd.GU7000_drawImage(image.width, image.height, image.data);
 }
