00e59adfe1540ab567446470fa17dac33cf2e213
Ben Klopfenstein <benklop@gmail.com>
2018-08-05 10:51:17 -0600

    disable pixel ring and vad led only

--- python/initialize.py
+++ python/initialize.py
@@ -8,10 +8,7 @@ wiringpi.wiringPiSetupGpio()
 
 #this script is responsible for initial setup and initialization of the enclosure's hardware.
 
-#first, disable the LEDs, since they are enable by default at power up
+#first, disable the ring LEDs, since they are enabled by default at power up
 pixel_ring.off()
-
-#next, reset the LCD, since it gets gibberish written to it during boot
-#reset pin is hooked up to GPIO 24.
-wiringpi.pinMode(24, wiringpi.GPIO.OUTPUT)
-wiringpi.pinMode(24, wiringpi.GPIO.INPUT)
+#disable the center LED too
+pixel_ring.set_vad_led(0)
